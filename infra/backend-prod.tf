dd4ac0edfe38b1aaa9d5c9fdec7092522a64de5e
Lester Jerome Ortiz <lesterjeromeortiz@gmail.com>
2025-10-20 21:19:57 +0800

    Delete infra/backend-prod.tf

--- infra/backend-prod.tf
+++
@@ -1,9 +0,0 @@
-terrform {
-  backend "s3" {
-    bucket       = "aws-simulated-org-tfstate"
-    key          = "infra/prod/terraform.tfstate"
-    region       = "ap-southeast-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
