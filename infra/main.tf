bda200b015f9f037e344314dea8ddc77877b7d48
lesterjerome <lesterjeromeortiz@gmail.com>
2025-11-01 22:11:17 +0800

    chore(infra): remove unused locals from main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,1 @@
-locals {
-  env_accounts = {
-    root  = module.accounts["root"]
-    dev   = module.accounts["dev"]
-    prod  = module.accounts["prod"]
-    audit = module.accounts["audit"]
-  }
-}
+
