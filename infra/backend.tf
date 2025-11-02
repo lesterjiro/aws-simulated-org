fd1d9fff35410289a4de20119b90bc7a0b9e2fe7
lesterjerome <lesterjeromeortiz@gmail.com>
2025-11-02 22:13:16 +0800

    refactor: remove providers.tf and backend.tf, move configurations to main.tf

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "aws-simulated-org-tfstate"
-    region       = "ap-southeast-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 
+  backend "s3" {
+    bucket       = "aws-simulated-org-tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_version = ">=1.5.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
